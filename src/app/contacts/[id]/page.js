8f55390451bc302761e3464edb009b7e54794d5d
limbanga <95570833+limbanga@users.noreply.github.com>
2024-05-06 14:05:39 +0700

    test use client

--- src/app/contacts/[id]/page.js
+++ src/app/contacts/[id]/page.js
@@ -1,15 +1,24 @@
-import Link from "next/link";
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const page = ({ params }) => {
-  console.log(params);
+  const router = useRouter();
   const { id } = params;
+
+  console.log(params);
+
+  const [count, setCount] = React.useState(0);
+
   return (
     <div>
       contact #{id}
-      <button>
-        <Link href="/contacts">Back</Link>
-      </button>
+      <Link href="/contacts">Back</Link>
+      <button onClick={() => router.replace("/contacts")}>Back</button>
+      <div>{count}</div>
+      <button className="bg-green-500" onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 };
--- src/app/contacts/page.js
+++ src/app/contacts/page.js
@@ -1,5 +1,15 @@
-export default function Home() {
+import Link from "next/link";
+
+const list = [1, 2, 3, 4, 5];
+
+export default function page() {
   return (
-    <div>home</div>
+    <div>
+      {list.map((item) => (
+        <div key={item}>
+          <Link href={`/contacts/${item}`}>Contact #{item}</Link>
+        </div>
+      ))}
+    </div>
   );
 }
